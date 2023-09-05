a9e9a71f511dc93194b84a708a689d3a2aefb899
mikhail_zakurdaev <zakurdaevm@gmail.com>
2023-09-05 17:27:27 +0300

    first spring and kotlin project

---
+++ src/test/kotlin/com/zakurdaevm/java/springboot/kotlinrest/KotlinrestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zakurdaevm.java.springboot.kotlinrest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinrestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
